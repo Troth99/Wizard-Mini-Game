ec94a2c965eb1cd4e6920a0143541c2df5a64e84
Troth99 <trothlol123456@gmail.com>
2025-02-28 08:46:45 +0200

    Animation Bar updated

--- engine.js
+++ engine.js
@@ -13,11 +13,13 @@ let player = {
 let game = {
     speed: 2,
     multiPly: 1.6,
+    fireBallMultiplier: 5
 };
 let scene = {
     score: 0,
 }
-function playingFunction(e){
+function playingFunction(e) {
+    const spells = document.getElementById('magic-bar')
     playBtn.style.display = 'none'
     gameScore.style.display = 'block'
     const mage = document.createElement('div');
@@ -25,47 +27,163 @@ function playingFunction(e){
     mage.style.position = "absolute"
     mage.style.top = '200px'
     mage.style.left = '200px'
-  
+    spells.style.display = 'flex'
+
 
     gameArea.appendChild(mage);
-    
+
 
     window.requestAnimationFrame(gameAction)
-    
+
 }
 
 let keys = {}
 document.addEventListener('keydown', onkeydown)
 document.addEventListener('keyup', onkeyup)
 
-function onkeydown(e){
+function onkeydown(e) {
     keys[e.code] = true
 }
 
-function onkeyup(e){
+function onkeyup(e) {
     keys[e.code] = false
 }
+let lastFireballTime = 0;
+const fireBallCooldown = 200;
 
-function gameAction(){
+let lastLightingTIme = 0;
+const lightingCooldown = 5000
+
+let isFireballOnCooldown = false;
+let isLightingOnCooldown = false
+function gameAction() {
     const wizzard = document.querySelector('.wizzard')
     const gameAreaRect = gameArea.getBoundingClientRect();
     const wizzardRect = wizzard.getBoundingClientRect()
-    if((keys.ArrowUp || keys.KeyW) && wizzardRect.top > gameAreaRect.top){
+    if ((keys.ArrowUp || keys.KeyW) && wizzardRect.top > gameAreaRect.top) {
         player.y -= game.speed * game.multiPly
     }
-    if((keys.ArrowDown || keys.KeyS) && wizzardRect.bottom < gameAreaRect.bottom){
+    if ((keys.ArrowDown || keys.KeyS) && wizzardRect.bottom < gameAreaRect.bottom) {
         player.y += game.speed * game.multiPly
     }
-    if((keys.ArrowLeft || keys.KeyA) && wizzardRect.left > gameAreaRect.left){
-        player.x -= game.speed * game.multiPly 
+    if ((keys.ArrowLeft || keys.KeyA) && wizzardRect.left > gameAreaRect.left) {
+        player.x -= game.speed * game.multiPly
     }
-    if((keys.ArrowRight || keys.KeyD)  && wizzardRect.right < gameAreaRect.right){
+    if ((keys.ArrowRight || keys.KeyD) && wizzardRect.right < gameAreaRect.right) {
         player.x += game.speed * game.multiPly
     }
+
+  
+    if (keys['Digit1'] && Date.now() - lastFireballTime > fireBallCooldown && !isFireballOnCooldown) {
+        isFireballOnCooldown = true
+        wizzard.classList.add('wizard-shoot')
+        addFIreBall(player)
+        lastFireballTime = Date.now()
+        setTimeout(() => {
+            wizzard.classList.remove('wizard-shoot');
+        }, 200);
+
+        setTimeout(() => {
+            isFireballOnCooldown = false
+        }, fireBallCooldown);
+    } 
+    if (keys['Digit2'] && Date.now() - lastLightingTIme > lightingCooldown && !isLightingOnCooldown) {
+        isLightingOnCooldown = true
+        wizzard.classList.add('wizard-shoot')
+        lighting(player)
+        lastFireballTime = Date.now()
+
+        setTimeout(() => {
+            wizzard.classList.remove('wizard-shoot');
+        }, 200);
+
+        setTimeout(() => {
+            isLightingOnCooldown = false
+        }, lightingCooldown);
+    } 
     scene.score++;
     gamePoints.textContent = scene.score
 
     wizzard.style.top = player.y + "px";
     wizzard.style.left = player.x + "px"
     window.requestAnimationFrame(gameAction)
+}
+
+const fireballSlot = document.querySelector('.magic-slot:first-child')
+const lightingSlot = document.querySelector('.magic-slot:nth-child(2)')
+
+function addFIreBall(player) {
+    if (fireballSlot.classList.contains('cooldown')) {
+        return;
+    }
+
+    isFireballOnCooldown = true;
+    let fireBall = document.createElement('div');
+    fireBall.classList.add('fire-ball');
+
+    const wizzard = document.querySelector('.wizzard');
+    const wizzardRect = wizzard.getBoundingClientRect();
+
+    fireBall.style.top = (player.y + wizzardRect.height / 3 - 5) + 'px';
+    fireBall.x = player.x + wizzardRect.width;
+    fireBall.style.left = fireBall.x + 'px';
+    gameArea.appendChild(fireBall);
+
+    fireballSlot.classList.add('cooldown');
+    setTimeout(() => {
+        fireballSlot.classList.remove('cooldown');
+        isFireballOnCooldown = false; 
+    }, 200);
+
+    function moveFireBall() {
+        fireBall.x += game.speed * 2;
+        fireBall.style.left = fireBall.x + 'px';
+
+        if (fireBall.x > gameArea.offsetWidth) {
+            fireBall.remove();
+        } else {
+            requestAnimationFrame(moveFireBall);
+        }
+    }
+
+    moveFireBall();
+}
+
+function lighting(player) {
+    if (lightingSlot.classList.contains('cooldown')) {
+        return;
+    }
+    isLightingOnCooldown = true
+    let lighting = document.createElement('div');
+    lighting.classList.add('lighting');
+
+    const wizzard = document.querySelector('.wizzard');
+    const wizzardRect = wizzard.getBoundingClientRect();
+
+    lighting.style.top = (player.y + wizzardRect.height * 0.04 )+ 'px';
+    lighting.x = player.x + wizzardRect.width;
+    lighting.style.left = lighting.x + 'px';
+
+    gameArea.appendChild(lighting);
+
+
+    lightingSlot.classList.add('cooldown');
+    setTimeout(() => {
+        lightingSlot.classList.remove('cooldown');
+        isLightingOnCooldown = false
+    }, 5000);
+
+
+    function moveLighting() {
+        lighting.x += game.speed * 3;
+        lighting.style.left = lighting.x + 'px';
+
+        if (lighting.x > gameArea.offsetWidth) {
+            lighting.remove();
+        } else {
+            requestAnimationFrame(moveLighting);
+        }
+    }
+
+    moveLighting();
 }
